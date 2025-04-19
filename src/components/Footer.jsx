7cb217505c1b44b615b7edc8e31a0918e80a87a6
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2025-04-20 04:33:12 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,7 +84,7 @@ const Footer = () => {
               <h6 className="text-lg font-bold mb-4 text-gray-800">Contact</h6>
               <ul className="space-y-2">
                 <li className="text-gray-700">
-                JCI SEN PDM D.V.S.P.GUPTA - Zone Chairman
+                  JCI SEN PDM D.V.S.P.GUPTA - Zone Chairman
                 </li>
                 <li>
                   <a
@@ -95,9 +95,8 @@ const Footer = () => {
                   </a>
                 </li>
                 <li>
-                  <a 
+                  <a
                     href="https://mail.google.com/mail/?view=cm&to=zsjaczone12@gmail.com&su=Hi%20i%20want%20to%20know%20more%20about%20your%20organization"
-
                     target="_blank"
                     rel="noopener noreferrer"
                     className="text-blue-700 hover:underline cursor-pointer"
@@ -124,10 +123,10 @@ const Footer = () => {
           </button>
 
           {/* Social Links */}
-          <nav>
+          <nav className="mr-20">
             <div className="flex gap-6">
               {/* Twitter */}
-              <a
+              {/* <a
                 href="https://twitter.com"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -142,11 +141,11 @@ const Footer = () => {
                 >
                   <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
                 </svg>
-              </a>
+              </a> */}
 
               {/* Facebook */}
               <a
-                href="https://facebook.com"
+                href="https://www.facebook.com/people/Jcialumniclub-India/pfbid0v7VUfB2m4UfUKR57X8RQ7MmBQ6xkCRnyxAeYyoyv2MiSFEvni7Ce6hsdB3jfPWK6l/?mibextid=9R9pXO"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:opacity-75 transition-opacity"
@@ -163,7 +162,7 @@ const Footer = () => {
               </a>
 
               {/* LinkedIn */}
-              <a
+              {/* <a
                 href="https://linkedin.com"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -178,7 +177,7 @@ const Footer = () => {
                 >
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
                 </svg>
-              </a>
+              </a> */}
             </div>
           </nav>
         </div>
