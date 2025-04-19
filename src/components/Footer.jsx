a218e85d55b14685a298a2952774451cfa5f842c
Anirudh Dattu <anirudh.dattu.22cse@bmu.edu.in>
2025-04-20 04:34:30 +0530

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -144,7 +144,7 @@ const Footer = () => {
               </a> */}
 
               {/* Facebook */}
-              <a
+              {/* <a
                 href="https://www.facebook.com/people/Jcialumniclub-India/pfbid0v7VUfB2m4UfUKR57X8RQ7MmBQ6xkCRnyxAeYyoyv2MiSFEvni7Ce6hsdB3jfPWK6l/?mibextid=9R9pXO"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -159,7 +159,7 @@ const Footer = () => {
                 >
                   <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
                 </svg>
-              </a>
+              </a> */}
 
               {/* LinkedIn */}
               {/* <a
